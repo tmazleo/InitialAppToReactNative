65a03ecd7086eca23cfc8ef2ba588ff2bfbebb70
tmazleo <leotomazb@icloud.com>
2023-08-28 22:29:14 -0300

    app estilizado

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native"
+import { View, Text, TextInput, TouchableOpacity } from "react-native"
 import ResultImc from "./ResultImc";
+import styles from "./style";
 
 export default function Form() {
 
@@ -32,28 +33,31 @@ export default function Form() {
         setMessageImc("preencha o peso e altura")
     }
     return (
-        <View>
-            <View>
+        <View style={styles.formContext}>
+            <View style={styles.form}>
 
-                <Text>Altura</Text>
+                <Text style={styles.formLabel}>Altura</Text>
                 <TextInput
+                 style={styles.formInput}
                     onChangeText={setHeight}
                     value={height}
                     placeholder="Ex. 1.75"
                     keyboardType="numeric"
                 />
 
-                <Text>Peso</Text>
+                <Text style={styles.formLabel}>Peso</Text>
                 <TextInput
+                style={styles.formInput}
                     onChangeText={setWeight}
                     value={weight}
                     placeholder="Ex. 75.365"
                     keyboardType="numeric"
                 />
-                <Button
-                onPress={() => validationImc()}
-                title={textButton} 
-                />
+                <TouchableOpacity
+                style={styles.buttonCalc}
+                onPress={() => {validationImc()}}>
+                <Text style={styles.textButtonCalc}>{textButton}</Text>
+                </TouchableOpacity>
             </View>
             <ResultImc messageResultImc={messageImc} resultImc={imc} />
             <View></View>
